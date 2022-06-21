40aa41e8cdca2cbecb1792403f6f5b085522dd82
poliana1905 <holopova.p@gmail.com>
2022-06-21 21:32:10 +0300

    тестовая ветка

--- BuildSim.Designer.cs
+++ BuildSim.Designer.cs
@@ -35,7 +35,7 @@ namespace SimulationBuilding
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.button1.Location = new System.Drawing.Point(91, 74);
+            this.button1.Location = new System.Drawing.Point(312, 161);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
